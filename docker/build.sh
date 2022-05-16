81d567440a754a32fc39d400e928beead63ea6e9
Yi Liu <yi.liu42@gmail.com>
2022-05-16 00:41:31 -0700

    deleted old files

--- docker/build.sh
+++
@@ -1,1 +0,0 @@
-docker build --network host -t mbtrex --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -f ./Dockerfile .
--- src/parser.py
+++
@@ -1,8 +0,0 @@
-from envs.maze import Maze
-
-
-def create_env(params):
-    if params["type"] == "maze":
-        return Maze(params["horizon"], params["use_images"], params["dense_rewards"], params["max_force"], params["goal_thresh"])
-    else:
-        raise NotImplementedError
