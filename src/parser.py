951e8b6469f665935e59ecb9982d156e4f889c46
Gaurav Datta <datta.gaurav@.gmail.com>
2022-05-31 21:48:56 -0700

    update parser

--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,10 @@
 from envs.maze import Maze
-
+from envs.hopper import Hopper
 
 def create_env(params):
     if params["type"] == "maze":
         return Maze(params["horizon"], params["use_images"], params["dense_rewards"], params["max_force"], params["goal_thresh"], params["walls"])
+    elif params["type"] == "hopper":
+        return Hopper(params)
     else:
         raise NotImplementedError
